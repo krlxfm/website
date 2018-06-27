63c73bb812e51fbf130729e7a72cefb7a5fe9924
Tate Bosler <tatebosler@gmail.com>
2018-06-26 22:08:27 -0500

    homepage css and layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,89 +5,66 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>KRLX</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="/css/app.css" rel="stylesheet">
+        <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <nav class="navbar navbar-expand-lg navbar-dark bg-krlx-red">
+            <a class="navbar-brand" href="#">KRLX</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Events</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Record Libe</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Playlist</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Schedule</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Mission Control</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="landing-card">
+            <img src="/storage/cover.jpg">
+            <div class="landing-headline px-3">
+                <h1 class="cover">Welcome to KRLX.</h1>
+                <h2>Don't worry, sometimes we mix up the letters too.</h2>
+                <p class="my-3 hide-mobile">
+                    <a href="#" class="btn btn-light btn-lg"><strong>Listen Live</strong></a>
+                    <a href="#" class="btn btn-outline-light">Request Song</a>
+                    <a href="#" class="btn btn-outline-light">Get Involved</a>
+                </p>
+            </div>
+        </div>
+        <div class="bg-black d-flex flex-wrap px-3 py-2 align-items-center">
+            <span class="onair-now"><strong>On air now:</strong> No Apologies <span class="text-smallcaps">with</span> Tate B.</span>
+            <span class="ml-auto">
+                <a href="#" class="btn btn-outline-light btn-sm mr-2">Listen live</a>
+                <a href="#" class="btn btn-outline-light btn-sm">About this show</a>
+            </span>
+        </div>
+        <div class="bg-dark text-light d-flex flex-wrap align-items-center px-3 py-1">
+            <span><strong>Up next, at 9:00 pm:</strong> ZootSuit Wyatt</span>
+            <span class="ml-auto">
+                <a href="#" class="text-light">Full schedule<i class="fas fa-chevron-right ml-1"></i></a>
+            </span>
+        </div>
+        <div class="container-fluid mt-4">
+            <div class="row">
+                <div class="col-md-3">
+                    <h2 class="text-center">Playlist</h2>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col-md-6">
+                    <h2 class="text-center">From the Blog</h2>
+                </div>
+                <div class="col-md-3">
+                    <h2 class="text-center">Events</h2>
                 </div>
             </div>
         </div>
