8d8c1869fc0ed729f6c73d2bb01dd7652e504186
Tate Bosler <tatebosler@gmail.com>
2018-06-28 22:02:19 -0500

    finish test - tracks successfully save

--- app/Track.php
+++ app/Track.php
@@ -2,9 +2,37 @@
 
 namespace KRLX;
 
+use KRLX\Events\TrackCreating;
 use Illuminate\Database\Eloquent\Model;
 
 class Track extends Model
 {
-    //
+    /**
+     * The attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description'
+    ];
+
+    /**
+     * The events that should be automatically dispatched.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'creating' => TrackCreating::class
+    ];
+
+    /**
+     * The attributes that should be type-cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'content' => 'array',
+        'scheduling' => 'array',
+        'etc' => 'array'
+    ];
 }
