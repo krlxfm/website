71913d8735b6fda07d3e4da0d26220468b5b0c31
Tate Bosler <tatebosler@gmail.com>
2018-06-28 21:10:37 -0500

    Created a few files to start the tracks, though technically this violates TDD

---
+++ app/Http/Controllers/API/TrackController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace KRLX\Http\Controllers\API;
+
+use KRLX\Track;
+use Illuminate\Http\Request;
+use KRLX\Http\Controllers\Controller;
+
+class TrackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \KRLX\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Track $track)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \KRLX\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Track $track)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \KRLX\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Track $track)
+    {
+        //
+    }
+}
---
+++ database/factories/TrackFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(KRLX\Track::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
