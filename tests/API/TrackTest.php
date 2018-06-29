a45f55313e746bc85664f22b78d447472fe1f15d
Tate Bosler <tatebosler@gmail.com>
2018-06-28 21:19:01 -0500

    Add blank test case file for track tests

---
+++ tests/API/TrackTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\API;
+
+use KRLX\Track;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TrackTest extends TestCase
+{
+
+}
