c905ed5e11ff2b004ebcd8c93bc8f89ab0f00883
Tate Bosler <tatebosler@gmail.com>
2018-06-28 21:52:24 -0500

    add to migration

--- database/migrations/2018_06_27_230006_create_tracks_table.php
+++ database/migrations/2018_06_27_230006_create_tracks_table.php
@@ -17,6 +17,45 @@ class CreateTracksTable extends Migration
             $table->increments('id');
             $table->timestamps();
             $table->softDeletes();
+            $table->boolean('active')->default(false);
+
+            $table->string('name');
+            $table->text('description');
+
+            // Bells and whistles
+            $table->boolean('boostable')->default(true);
+            $table->boolean('clonable')->default(true);
+            $table->boolean('allows_images')->default(true);
+            $table->boolean('can_fall_back')->default(true);
+            $table->boolean('joinable')->default(true);
+            $table->boolean('taggable')->default(true);
+            $table->boolean('awards_xp')->default(true);
+
+            // Priority
+            $table->string('prefix')->nullable();
+            $table->char('zone', 1)->nullable();
+            $table->tinyInteger('group')->unsinged()->nullable();
+            $table->smallInteger('order')->unsigned()->default(1000);
+
+            // Group size and participants
+            $table->boolean('allows_direct_add')->default(false);
+            $table->tinyInteger('max_participants')->unsigned()->nullable();
+
+            // Content
+            $table->string('title_label')->nullable();
+            $table->string('description_label')->nullable();
+            $table->smallInteger('description_min_length')->unsigned()->nullable();
+            $table->text('content');
+
+            // Scheduling
+            $table->boolean('weekly')->default(true);
+            $table->enum('start_day', ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])->nullable();
+            $table->string('start_time')->nullable();
+            $table->string('end_time')->nullable();
+            $table->text('scheduling');
+
+            // Additional questions
+            $table->text('etc');
         });
     }
 
