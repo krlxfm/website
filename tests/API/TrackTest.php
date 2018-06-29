3917c04d1eaeffe397a00f832b28df58d514ab25
Tate Bosler <tatebosler@gmail.com>
2018-06-28 21:27:24 -0500

    add test suite to phpunit and start greenifying test

--- tests/API/TrackTest.php
+++ tests/API/TrackTest.php
@@ -9,5 +9,24 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TrackTest extends TestCase
 {
+    use RefreshDatabase;
 
+    /**
+     * Test that tracks can be created.
+     * Given a track name and description, assert that the track is created,
+     * and that the API call returns a status code 201.
+     *
+     * @return void
+     */
+    public function testTrackCreation()
+    {
+        $trackData = [
+            'name' => 'Epic Shows',
+            'description' => 'A track only for epic shows. Shows will be rejected if they are not epic.'
+        ];
+
+        $request = $this->json('POST', '/api/v1/tracks', $trackData);
+
+        $request->assertStatus(201);
+    }
 }
