a753bc17326746bb2e0889796a5a4d79f17e1acd
Tate Bosler <tatebosler@gmail.com>
2018-07-04 15:47:57 -0500

    test that going to /register without an email sends you to /login

---
+++ tests/Browser/RegistrationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\Login\Registration;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegistrationTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Registration)
+                    ->assertPathIs('/login');
+        });
+    }
+}
