3a7af04e75054e65d5baa4726b02475214884429
Tate Bosler <tatebosler@gmail.com>
2018-07-07 00:15:22 -0500

    move guest tests into guest file

---
+++ tests/API/APITestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\API;
+
+use KRLX\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class APITestCase extends TestCase
+{
+    use RefreshDatabase;
+
+    public $user;
+    public $session;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->session = $this->actingAs($this->user, 'api');
+    }
+}
