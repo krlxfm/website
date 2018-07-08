7e8af4e1b838a35537f446e766c9ddefbf67ee7b
Tate Bosler <tatebosler@gmail.com>
2018-07-07 23:51:25 -0500

    add lots of validation rules

--- app/Http/Requests/ShowUpdateRequest.php
+++ app/Http/Requests/ShowUpdateRequest.php
@@ -2,6 +2,8 @@
 
 namespace KRLX\Http\Requests;
 
+use KRLX\Track;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ShowUpdateRequest extends FormRequest
@@ -23,10 +25,114 @@ class ShowUpdateRequest extends FormRequest
      */
     public function rules()
     {
-        $track = $this->show->track;
-        return [
+        $track = Track::find($this->input('track_id')) ?? $this->show->track;
+        return array_merge(
+            $this->baseRules(),
+            $this->trackDependentRules($track),
+            $this->customFieldRules($track)
+        );
+    }
+
+    /**
+     * Returns the "base" rules, applicable to all tracks.
+     *
+     * @return array
+     */
+    protected function baseRules()
+    {
+        $special_times = array_keys(config('defaults.special_times'));
+        $baseRules = [
+            'track_id' => ['integer', Rule::exists('tracks', 'id')->where(function ($query) {
+                $query->where('active', true);
+            })],
+            'submitted' => ['boolean'],
             'title' => ['string', 'min:3', 'max:200'],
-            'description' => ['string']
+            'content' => ['array'],
+            'scheduling' => ['array'],
+            'conflicts' => ['array', 'min:0'],
+            'preferences' => ['array', 'min:1'],
+            'etc' => ['array'],
+            'special_times' => ['array', 'size:'.count($special_times)],
+            'classes.*' => ['string'],
+            'tags.*' => ['string'],
+            'preferred_length' => ['integer', 'min:0', 'max:240'],
+            'notes' => ['nullable', 'string', 'max:65000'],
+            'date' => ['nullable', 'date'],
+            'day' => ['nullable', 'string', 'in:Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday'],
+            'start' => ['nullable', 'string', 'regex:([01][0-9]|2[0-3]):[0-5][0-9]'],
+            'end' => ['nullable', 'string', 'regex:([01][0-9]|2[0-3]):[0-5][0-9]']
         ];
+
+        foreach($special_times as $time) {
+            $baseRules["special_times.$time"] = ['string', 'in:y,m,n'];
+        }
+
+        return $baseRules;
+    }
+
+    /**
+     * Returns the basic which change based on the track settings.
+     * (These aren't the custom fields.)
+     *
+     * @param  KRLX\Track  $track
+     * @return array
+     */
+    protected function trackDependentRules(Track $track)
+    {
+        $trackDepRules = [
+            'description' => ['string', 'min:'.$track->description_min_length, 'max:65000'],
+            'classes' => ['array', ($track->weekly ? 'min:1' : 'max:0')],
+            'conflicts.*' => ($track->weekly ? ['array'] : ['date', 'distinct']),
+            'preferences.*' => ($track->weekly ? ['array'] : ['date', 'distinct']),
+            'tags' => ['array', ($track->taggable ? 'min:0' : 'max:0')]
+        ];
+
+        if($track->weekly) {
+            $trackDepRules['conflicts.*.days'] = ['array', 'min:1', 'max:7'];
+            $trackDepRules['conflicts.*.days.*'] = ['string', 'distinct', 'in:Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday'];
+            $trackDepRules['conflicts.*.start'] = ['string', 'regex:([01][0-9]|2[0-3]):[03]0'];
+            $trackDepRules['conflicts.*.end'] = ['string', 'regex:([01][0-9]|2[0-3]):[03]0'];
+            $trackDepRules['preferences.*.days'] = ['array', 'min:1', 'max:7'];
+            $trackDepRules['preferences.*.days.*'] = ['string', 'distinct', 'in:Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday'];
+            $trackDepRules['preferences.*.start'] = ['string', 'regex:([01][0-9]|2[0-3]):[03]0'];
+            $trackDepRules['preferences.*.end'] = ['string', 'regex:([01][0-9]|2[0-3]):[03]0'];
+            $trackDepRules['preferences.*.strength'] = ['integer', 'min:0', 'max:200'];
+        }
+
+        return $trackDepRules;
+    }
+
+    /**
+     * Returns the rules for custom fields.
+     *
+     * @param  KRLX\Track  $track
+     * @return array
+     */
+    protected function customFieldRules(Track $track)
+    {
+        $custom = ['content', 'scheduling', 'etc'];
+        $rules = [];
+        foreach($custom as $category) {
+            foreach($track->{$category} as $field) {
+                $rules[$category.'.'.$field['db']] = $this->processCustomFieldRules($field['rules']);
+            }
+        }
+
+        return $rules;
+    }
+
+    /**
+     * Process the validation rules for a given custom field.
+     *
+     * @param  array  $rules
+     * @return array
+     */
+    protected function processCustomFieldRules(array $rules)
+    {
+        $filtered_rules = array_where($rules, function($value, $key) {
+            return $value != 'required';
+        });
+
+        return $filtered_rules;
     }
 }
