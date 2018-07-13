f0b5a12440617c54f305ac6582e4a8a26f9c61dc
Tate Bosler <tatebosler@gmail.com>
2018-07-12 22:55:11 -0500

    try to make the page look decent on an iPhone X

--- resources/views/layouts/missioncontrol.blade.php
+++ resources/views/layouts/missioncontrol.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -15,6 +15,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/etc/iphonex.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 </head>
 <body class="bg-gray {{ $body_class ?? '' }}">
@@ -86,7 +87,7 @@
         <div class="container">
             @yield('foot')
         </div>
-        @yield('bottom')
     </div>
+    @yield('bottom')
     @stack('js')
 </body>
