c8a7b7a4e7a15a61349d397052ce409687b1f55a
Tate Bosler <tatebosler@gmail.com>
2018-07-17 22:39:54 -0500

    add stuff to the home screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,5 @@
         <a href="{{ route('shows.create') }}" class="btn btn-primary">Create new radio show</a>
     </p>
     <p>(Sorry this doesn't look super glamorous! It should look better soon!)</p>
+    <p>You are currently testing on Beta 1 - v0.5.3 "Wilson IV" - <a href="https://github.com/krlxfm/website/releases/tag/v0.5.3">changelog</a></p>
 @endsection
--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -23,18 +23,21 @@
             <h3>While testing, keep in mind:</h3>
             <ul>
                 <li><strong>THIS IS A BETA.</strong> It should work reasonably well, but don't expect a flawless experience (especially with regards to cosmetics).</li>
-                <li>The point of this beta is to verify that individual shows can be created and saved correctly and without too much of a hassle.</li>
+                <li>The point of this beta is to verify that individual shows can be created and saved correctly and without too much of a hassle. While you are welcome to enter your full schedule into your show applications to test that it all works, don't worry about getting it perfect. <strong>Applications created here will not automatically transfer over to the official set.</strong></li>
                 <li><strong>Group shows sort of work, but not really.</strong> As such, please create your shows as a solo venture. You'll see the group invite process, but please ignore it and carry on directly into content. Group shows should be working much better during Beta 2.</li>
+                <li>There is currently no way to delete show applications. If you need to delete one or withdraw it from "submitted" status, let me know. This should be fixed in a future update.</li>
+                <li>You win if you break something.</li>
             </ul>
 
             <h3>When something breaks, let me know!</h3>
             <p>You can send me feedback and bug reports via email. Or, if you have a GitHub account, feel free to <a href="https://github.com/krlxfm/website/issues">open an issue on the GitHub project.</a></p>
+            <p>Anyone who reports a bug will get a shout-out in <a href="https://github.com/krlxfm/website/releases">the release that fixes it.</a></p>
 
             <h3>Entry into A1 priority drawing and Fall 2018 early access</h3>
             <p>You'll need to do <strong>BOTH</strong> of the following in order to get early access to the Fall 2018 applications, and to get entered into the drawing for A1 priority:</p>
             <ul>
                 <li>Create and submit at least one Standard show, and one show on a different track.</li>
-                <li>Let me know how it went by filling out the feedback survey.</li>
+                <li>Let me know how it went by <a href="http://go.tatebosler.com/krlx-beta1">filling out the feedback survey.</a></li>
             </ul>
             <p>Of course, if you have any questions or run into problems, please get in touch!</p>
 
