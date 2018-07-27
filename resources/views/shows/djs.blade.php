d680c2f036f2d0b7078a4a9dd5925e61d5883c49
Tate Bosler <tatebosler@gmail.com>
2018-07-26 21:23:44 -0500

    properly end closing tags and remove invitations on dj list

--- resources/views/shows/djs.blade.php
+++ resources/views/shows/djs.blade.php
@@ -41,10 +41,9 @@
                                 <h5 class="mb-1">{{ $user->full_name }}</h5>
                                 <ul class="mb-0">
                                     @foreach($user->shows as $show)
-                                        <li><a href="{{ route('shows.review', $show) }}">{{ $show->title }}</li>
-                                    @endforeach
-                                    @foreach($user->invitations as $show)
-                                        <li><a href="{{ route('shows.review', $show) }}">{{ $show->title }}</a> <small class="text-muted">(invitation pending)</small></li>
+                                        <li>
+                                            <a href="{{ route('shows.review', $show) }}">{{ $show->title }}</a>
+                                        </li>
                                     @endforeach
                                 </ul>
                             </td>
