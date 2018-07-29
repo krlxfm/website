0f1111dce6a21991a00070fd543092911ca8bb9e
Tate Bosler <tatebosler@gmail.com>
2018-07-29 12:47:03 -0500

    display errors to user on submission

--- resources/assets/js/pages/shows/review.js
+++ resources/assets/js/pages/shows/review.js
@@ -12,12 +12,40 @@ function reviewAndSubmit() {
         if(!choice) throw new Error();
     })
     .then(() => {
-        axios.patch('/api/v1/shows/'+showID, {
+        axios.put('/api/v1/shows/'+showID+'/submitted', {
             submitted: true
         })
         .then((response) => {
-            window.location.reload();
+            console.log(response);
+        })
+        .catch((err) => {
+            showErrors(err.response.data.errors);
         })
     })
     .catch((err) => {});
+}
+
+function showErrors(errors) {
+    var list = document.createElement('ul');
+    for(var field in errors) {
+        var errMessage = errors[field][0];
+        if(field.indexOf('.') != -1) {
+            var components = field.split('.');
+            errMessage = errMessage.replace(components[0] + '.', '');
+        }
+        var item = document.createElement('li');
+        var newContent = document.createTextNode(errMessage);
+        item.style.textAlign = 'left';
+        item.appendChild(newContent);
+        list.appendChild(item);
+    }
+
+    var errorLength = Object.keys(errors).length;
+
+    return swal({
+        title: "Whoops!",
+        text: (errorLength == 1 ? 'One error prevented the show from being submitted. Please fix it and try again.' : errorLength + " errors prevented the show from being submitted. Please fix them and try again."),
+        content: list,
+        icon: "error"
+    })
 }
