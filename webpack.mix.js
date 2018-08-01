410cf67effbbaa93078c463f1db08962d25d8116
Tate Bosler <tatebosler@gmail.com>
2018-07-31 21:34:52 -0500

    start work on asset versioning - #81

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,8 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .copyDirectory('resources/assets/js/pages', 'public/js/pages');
+
+if (mix.inProduction()) {
+    mix.version();
+    mix.disableNotifications();
+}
