edcaedc049d1775d64778666fa3806ba260444be
Tate Bosler <tatebosler@gmail.com>
2018-08-04 17:43:03 -0500

    allow mass setting of user year

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'first_name', 'photo',
+        'name', 'email', 'password', 'first_name', 'photo', 'year'
     ];
 
     /**
