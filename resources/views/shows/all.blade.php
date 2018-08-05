c6be14cbd6b7761295b493adeeb5813c0827ae31
Tate Bosler <tatebosler@gmail.com>
2018-08-04 22:57:46 -0500

    do stuff to the master show list

--- resources/views/shows/all.blade.php
+++ resources/views/shows/all.blade.php
@@ -29,7 +29,13 @@
                             <td class="align-middle">
                                 <h5 class="mb-0">{{ $show->title }}</h5>
                                 <small class="text-muted">
-                                    {{ $show->id }} | {{ $show->track->name }} | last updated {{ $show->updated_at->toDayDateTimeString() }}
+                                    {{ $show->id }} | {{ $show->track->name }}
+                                    @if($show->day and $show->start and $show->end)
+                                        |
+                                        <span class="text-primary">
+                                            {{ $show->day }}, {{ Carbon\Carbon::createFromTimeString($show->start)->format('g:i a') }} &ndash; {{ Carbon\Carbon::createFromTimeString($show->end)->format('g:i a') }}
+                                        </span>
+                                    @endif
                                 </small>
                                 <br>
                                 {{ $show->description }}
@@ -102,49 +108,51 @@
                     </tbody>
                 </table>
             @endforeach
-            <div class="d-flex mb-3 mt-5 flex-wrap align-items-center">
-                <h2>Incomplete Shows</h2>
-                <div class="btn-group ml-auto">
-                    <a href="#" class="btn btn-primary"><i class="fas fa-bell"></i> Remind shows</a>
+            @if($shows->where('submitted', false)->count() > 0)
+                <div class="d-flex mb-3 mt-5 flex-wrap align-items-center">
+                    <h2>Incomplete Shows</h2>
+                    <div class="btn-group ml-auto">
+                        <a href="#" class="btn btn-primary"><i class="fas fa-bell"></i> Remind shows</a>
+                    </div>
                 </div>
-            </div>
-            <table class="table table-borderless table-striped">
-                <thead>
-                    <tr>
-                        <th class="w-50">Title and details</th>
-                        <th>Hosts</th>
-                        <th>Priority</th>
-                        <th style="width: 171px">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($shows->where('submitted', false) as $show)
+                <table class="table table-borderless table-striped">
+                    <thead>
                         <tr>
-                            <td class="align-middle">
-                                <h5 class="mb-0">{{ $show->title }} <small><span class="badge badge-warning">INCOMPLETE</span></small></h5>
-                                <small class="text-muted">
-                                    {{ $show->id }} | {{ $show->track->name }} | last updated {{ $show->updated_at->toDayDateTimeString() }}
-                                </small>
-                                <br>
-                                {{ $show->description }}
-                            </td>
-                            <td class="align-middle">
-                                {!! implode('<br>', $show->hosts->pluck('full_name')->all()) !!}
-                            </td>
-                            <td class="align-middle text-center bg-priority-{{ strtolower($show->priority[0]) }}">
-                                {{ $show->track->prefix }}
-                                {{ $show->priority }}
-                            </td>
-                            <td class="align-middle">
-                                <div class="btn-group">
-                                    <a href="{{ route('shows.review', $show) }}" class="btn btn-outline-primary">View/Edit</a>
-                                    <a href="{{ route('shows.review', $show) }}" class="btn btn-outline-danger">Delete</a>
-                                </div>
-                            </td>
+                            <th class="w-50">Title and details</th>
+                            <th>Hosts</th>
+                            <th>Priority</th>
+                            <th style="width: 171px">Actions</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach($shows->where('submitted', false) as $show)
+                            <tr>
+                                <td class="align-middle">
+                                    <h5 class="mb-0">{{ $show->title }} <small><span class="badge badge-warning">INCOMPLETE</span></small></h5>
+                                    <small class="text-muted">
+                                        {{ $show->id }} | {{ $show->track->name }} | last updated {{ $show->updated_at->toDayDateTimeString() }}
+                                    </small>
+                                    <br>
+                                    {{ $show->description }}
+                                </td>
+                                <td class="align-middle">
+                                    {!! implode('<br>', $show->hosts->pluck('full_name')->all()) !!}
+                                </td>
+                                <td class="align-middle text-center bg-priority-{{ strtolower($show->priority[0]) }}">
+                                    {{ $show->track->prefix }}
+                                    {{ $show->priority }}
+                                </td>
+                                <td class="align-middle">
+                                    <div class="btn-group">
+                                        <a href="{{ route('shows.review', $show) }}" class="btn btn-outline-primary">View/Edit</a>
+                                        <a href="{{ route('shows.review', $show) }}" class="btn btn-outline-danger">Delete</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
         </div>
     </div>
 @endsection
