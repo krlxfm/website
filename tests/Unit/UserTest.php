3faad01c6b14162561a70762887c355be772052c
Tate Bosler <tatebosler@gmail.com>
2018-08-05 22:15:19 -0500

    ugh notifications why

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -5,9 +5,7 @@ namespace Tests\Unit;
 use KRLX\Term;
 use KRLX\User;
 use Tests\TestCase;
-use KRLX\Notifications\ResetPassword;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Notification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
@@ -20,18 +18,6 @@ class UserTest extends TestCase
     {
         parent::setUp();
         $this->user = factory(User::class)->states('contract_ok')->create();
-    }
-
-    /**
-     * Test that users can request a password reset.
-     *
-     * @return void
-     */
-    public function testPasswordResetRequest()
-    {
-        Notification::fake();
-        $this->withSession(['email', $this->user->email])->get('/password/reset');
-        Notification::assertSentTo($this->user, ResetPassword::class);
     }
 
     /**
