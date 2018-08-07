c1b8c7b438865077f8d694578ed2944f9997a2d3
Tate Bosler <tatebosler@gmail.com>
2018-08-07 17:56:14 -0500

    add more information to the Queue

--- resources/assets/js/components/schedule/Queue.vue
+++ resources/assets/js/components/schedule/Queue.vue
@@ -3,7 +3,20 @@
         <h5 class="card-header">Queue</h5>
         <ul class="list-group list-group-flush">
             <li class="list-group-item" v-for="show in shows">
-                {{ show.title }}
+                <h5 class="head-sans-serif mb-0">{{ show.title }}</h5>
+                <p class="mb-0">
+                    <span class="badge" v-bind:class="'bg-priority-'+show.priority.charAt(0).toLowerCase()">
+                        {{ show.priority }}
+                    </span>
+                    <span class="badge bg-priority-a">
+                        <i class="fas" v-bind:class="show.hosts.length == 1 ? 'fa-user' : 'fa-users'"></i>
+                        {{ show.hosts.length }}
+                    </span>
+                    <span class="badge" v-bind:class="lengthColors[show.preferred_length / 30]">
+                        <i class="fas fa-clock"></i>
+                        {{ show.preferred_length }}m
+                    </span>
+                </p>
             </li>
         </ul>
     </div>
@@ -11,6 +24,11 @@
 
 <script>
 module.exports = {
+    data: function() {
+        return {
+            lengthColors: ['badge-light', 'badge-primary', 'badge-success', 'badge-warning', 'badge-danger']
+        }
+    },
     props: {
         shows: Array
     }
