c06e746a56215c8db55830dce9ae91fb9ee09b35
Tate Bosler <tatebosler@gmail.com>
2018-08-07 20:51:30 -0500

    add extra data to events

--- resources/assets/js/schedule.js
+++ resources/assets/js/schedule.js
@@ -11,6 +11,20 @@ $(document).ready(function() {
     enableDragging();
 });
 
+const priorityColors = {
+    "a": '#1b1c1d',
+    "b": '#767676',
+    "c": '#6435c9',
+    "d": '#2185d0',
+    "e": '#00b5ad',
+    "f": '#21ba45',
+    "g": '#b5cc18',
+    "h": '#fbbd08',
+    "i": '#f2711c',
+    "j": '#db2828',
+    "s": '#e8e8e8'
+};
+
 function setupCalendar() {
     $("#calendar").fullCalendar({
         header: false,
@@ -29,18 +43,20 @@ function setupCalendar() {
 window.enableDragging = function() {
     $('.schedule-queue-item').each(function() {
         const showID = $(this).data('showId');
-        // store data so the calendar knows to render an event upon drop
+        const show = showList[showID];
+
         $(this).data('event', {
             id: showID,
-            title: showList[showID].title,
-            stick: true
+            title: show.title,
+            color: priorityColors[show.priority.charAt(0).toLowerCase()],
+            textColor: ['g', 'h', 's'].includes(show.priority.charAt(0).toLowerCase()) ? 'black' : 'white',
+            duration: '0'+Math.floor(show.preferred_length / 60)+':'+((show.preferred_length % 60) / 10)+'0'
         });
 
-        // make the event draggable using jQuery UI
         $(this).draggable({
             zIndex: 999,
-            revert: true,      // will cause the event to go back to its
-            revertDuration: 0  //  original position after the drag
+            revert: true,
+            revertDuration: 0
         });
     });
 }
