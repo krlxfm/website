85c751e38c1cefccab1c2df3b81ac7b6f41307ac
Tate Bosler <tatebosler@gmail.com>
2018-08-12 23:11:14 -0500

    since the text is smaller, we can make those bigger

--- resources/assets/js/schedule.js
+++ resources/assets/js/schedule.js
@@ -29,7 +29,7 @@ function setupCalendar() {
     $("#calendar").fullCalendar({
         header: false,
         defaultView: 'agendaWeek',
-        columnHeaderFormat: 'ddd',
+        columnHeaderFormat: 'dddd',
         allDaySlot: false,
         businessHours: {dow: [0, 1, 2, 3, 4, 5, 6], start: '06:00', end: '22:00'},
         now: moment().subtract(7, 'd'),
