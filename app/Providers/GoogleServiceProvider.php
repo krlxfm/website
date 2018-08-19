a3db18719e26883f4a5839ed710733645d968dc9
Tate Bosler <tatebosler@gmail.com>
2018-08-19 12:15:48 -0500

    bind Google into the service container

---
+++ app/Providers/GoogleServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace KRLX\Providers;
+
+use Google_Client;
+use Google_Service_Calendar;
+use Illuminate\Support\ServiceProvider;
+
+class GoogleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $client = new Google_Client();
+        $client->setApplicationName(config('app.name'));
+        $client->setScopes(implode(' ', [Google_Service_Calendar::CALENDAR]));
+        $client->setClientId(config('services.google_calendar.client_id'));
+        $client->setClientSecret(config('services.google_calendar.client_secret'));
+        $client->setRedirectUri(config('services.google_calendar.redirect'));
+        $client->setAccessType('offline');
+
+        $credentialsPath = storage_path('google.json');
+        if (file_exists($credentialsPath)) {
+            $accessToken = json_decode(file_get_contents($credentialsPath), true);
+            $client->setAccessToken($accessToken);
+            if ($client->isAccessTokenExpired()) {
+                $refreshToken = $client->getRefreshToken();
+                $accessToken = $client->fetchAccessTokenWithRefreshToken($refreshToken);
+                $accessToken["refresh_token"] = $refreshToken;
+                file_put_contents($credentialsPath, json_encode($accessToken));
+            }
+        }
+
+        $this->app->instance('Google_Client', $client);
+    }
+}
