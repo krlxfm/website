77e12a52acefe48ed351ba7bb259f33b5aaa389d
Tate Bosler <tatebosler@gmail.com>
2018-08-19 14:07:31 -0500

    fix regex

--- app/Http/Controllers/API/ScheduleController.php
+++ app/Http/Controllers/API/ScheduleController.php
@@ -17,9 +17,9 @@ class ScheduleController extends Controller
     public function update(Request $request, Show $show)
     {
         $request->validate([
-            'day' => ['nullable', 'string', 'in:Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday'],
-            'start' => ['nullable', 'string', 'regex:([01][0-9]|2[0-3]):[0-5][0-9]'],
-            'end' => ['nullable', 'string', 'regex:([01][0-9]|2[0-3]):[0-5][0-9]'],
-        ])
+            'day' => ['sometimes', 'string', 'in:Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday'],
+            'start' => ['sometimes', 'string', 'regex:/([01][0-9]|2[0-3]):[0-5][0-9]/'],
+            'end' => ['sometimes', 'string', 'regex:/([01][0-9]|2[0-3]):[0-5][0-9]/'],
+        ]);
     }
 }
