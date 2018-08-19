f8cf04e003d5c825557197c23a1809a85e4ffb3e
Tate Bosler <tatebosler@gmail.com>
2018-08-19 16:57:41 -0500

    write test for publication job handling

--- tests/API/ScheduleTest.php
+++ tests/API/ScheduleTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\API;
 
+use KRLX\Term;
 use KRLX\Show;
+use KRLX\Jobs\PublishShow;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ScheduleTest extends APITestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     /**
      * Test an update to a show's scheduling data.
@@ -30,5 +34,36 @@ class ScheduleTest extends APITestCase
         $this->assertEquals('Sunday', $show_updated->day);
         $this->assertEquals('12:00', $show_updated->start);
         $this->assertEquals('13:00', $show_updated->end);
+    }
+
+    /**
+     * Test that when we call the "publish" route, that jobs are dispatched to
+     * the queue for processing.
+     *
+     * @return void
+     */
+    public function testPublishingDispatchesJobs()
+    {
+        Queue::fake();
+        $faker = $this->faker();
+
+        $term = factory(Term::class)->create(['accepting_applications' => true]);
+        $shows = factory(Show::class, 10)->create([
+            'term_id' => $term->id,
+            'start' => $faker->time('H:i'),
+            'end' => $faker->time('H:i'),
+            'day' => $faker->date('l'),
+        ]);
+
+        $publish_array = array_fill_keys($shows->pluck('id')->all(), 'new');
+        $request = $this->json('PATCH', '/api/v1/schedule/publish', $publish_array);
+        $request->assertOk();
+
+        Queue::assertPushed(PublishShow::class, 10);
+        foreach($shows as $show) {
+            Queue::assertPushed(PublishShow::class, function ($job) use ($show) {
+                return $job->show->id === $show->id;
+            });
+        }
     }
 }
