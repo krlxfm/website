7912ac8f4088ad98d332e73d64ea6647591fa4f0
Tate Bosler <tatebosler@gmail.com>
2018-09-09 21:30:50 -0500

    remove close button while publication is in progress

--- resources/assets/js/components/schedule/Publisher.vue
+++ resources/assets/js/components/schedule/Publisher.vue
@@ -50,9 +50,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="publishStatus-label">Publication in progress</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
                 </div>
                 <div class="modal-body">
                     <p>{{ currentItem }}</p>
