54ebe69737324751b856966b825cd99bd602b172
Tate Bosler <tatebosler@gmail.com>
2018-09-10 19:56:16 -0500

    why is that there, that's dumb

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -5,7 +5,7 @@
         <div class="col">
             <h1>Action Forbidden</h1>
 
-            <p>You attempted to take a prohibited action or visit a page which you do not have access to. This is likely because the page you attempted to view is restricted. Not to worry though, no data has been affected.  </p>
+            <p>You attempted to take a prohibited action or visit a page which you do not have access to. This is likely because the page you attempted to view is restricted. Not to worry though, no data has been affected.</p>
             <p>If you believe this is an error, please contact an IT engineer for assistance.</p>
             <h3>Back to safety</h3>
             <ul>
