17f77e4d2cf4dd01e9f5270368da33e07a34412d
Tate Bosler <tatebosler@gmail.com>
2018-09-11 22:33:39 -0500

    fix page being misnamed

--- resources/views/shows/find.blade.php
+++ resources/views/shows/find.blade.php
@@ -4,7 +4,7 @@
     <div class="row my-3">
         <div class="col">
             <div class="d-flex flex-wrap align-items-center">
-                <h1>Create New Show</h1>
+                <h1>Join Show</h1>
                 <a href="{{ route('shows.my') }}" class="btn btn-outline-secondary ml-auto"><i class="fas fa-chevron-left"></i> Back to My Shows</a>
             </div>
         </div>
@@ -13,7 +13,7 @@
         <div class="col">
             <div class="card">
                 <div class="card-body py-5 px-4 text-center">
-                    <p>Enter the ID of the show you would like to join (this is a {{ config('defaults.show_id_length', 6) }}-character string of capital letters and numbers).</p>
+                    <p>Enter the ID of the show you would like to join (this is a string of {{ config('defaults.show_id_length', 6) }} capital letters and numbers).</p>
                     <form id="id-search-form">
                         <div class="input-group input-group-lg">
                             <input class="form-control text-center" type="text" placeholder="Show ID" id="id-search">
