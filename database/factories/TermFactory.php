222fa036db02c619a02c931feb22fdb1cc506146
Tate Bosler <tatebosler@gmail.com>
2018-09-21 21:21:20 -0500

    tests pass

--- database/factories/TermFactory.php
+++ database/factories/TermFactory.php
@@ -6,9 +6,11 @@ use Faker\Generator as Faker;
 $factory->define(KRLX\Term::class, function (Faker $faker) {
     $date = $faker->dateTimeThisYear();
     $futureDate = Carbon::instance($date);
+    $closeDate = $futureDate->copy();
 
     return [
         'id' => date('Y').'-'.$faker->regexify('[A-Z0-9][A-Z0-9_]+[A-Z0-9]'),
+        'applications_close' => $closeDate->subDay(),
         'on_air' => $date,
         'off_air' => $futureDate->addWeeks(2),
         'boosted' => true,
