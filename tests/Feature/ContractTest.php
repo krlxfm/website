ce8efd2f6f94d4e1cfd4ca923fae543e57c6fa5e
Tate Bosler <tatebosler@gmail.com>
2018-10-03 22:36:31 -0500

    more testing

--- tests/Feature/ContractTest.php
+++ tests/Feature/ContractTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use KRLX\Show;
 use KRLX\Term;
 use Tests\AuthenticatedTestCase;
 
@@ -22,10 +23,17 @@ class ContractTest extends AuthenticatedTestCase
      */
     public function testUsersMustSignContract()
     {
-        $this->assertCount(0, $this->carleton->points->where('term_id', $this->term->id));
+        $show = factory(Show::class)->create([
+            'term_id' => $this->term->id,
+        ]);
 
-        $request = $this->actingAs($this->carleton)->get('/shows/join');
-        $request->assertRedirect('/contract');
+        $direct_request = $this->actingAs($this->carleton)->get('/shows/join');
+        $show_join_request = $this->actingAs($this->carleton)->get("/shows/join/{$show->id}");
+
+        $this->assertCount(0, $this->carleton->points->where('term_id', $this->term->id));
+        $direct_request->assertRedirect('/contract');
+        $show_join_request->assertRedirect('/contract')
+                          ->assertSessionHas('term', $this->term->id);
     }
 
     /**
@@ -38,6 +46,7 @@ class ContractTest extends AuthenticatedTestCase
     {
         $request = $this->actingAs($this->carleton)->get('/contract');
         $request->assertOk()
+                ->assertSessionMissing('term')
                 ->assertSee($this->term->id);
     }
 }
