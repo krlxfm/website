17e3a8ab302fa74f2a6bb5924c492dc086cbbede
Tate Bosler <tatebosler@gmail.com>
2018-10-14 13:15:48 -0500

    fix travis

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->tinyInteger('order')->unsigned()->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
