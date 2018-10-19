7af9d6473c70e737e3b1c0440d73d07fa42773d1
Tate Bosler <tatebosler@gmail.com>
2018-10-19 14:03:18 -0500

    test config defaults

--- app/Config.php
+++ app/Config.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Config extends Model
 {
-    //
+    protected $fillable = ['name', 'value'];
+
+    /**
+     * The preferred way of getting configuration values is to use
+     * Config::valueOr(). If the key given does not exist, the provided
+     * "fallback" value will be returned instead.
+     *
+     * @param  string  $key
+     * @param  string|null  $default
+     * @return string|null
+     */
+    public static function valueOr(string $key, string $default = null)
+    {
+        $config = self::where('name', $key)->first();
+        if ($config) {
+            return $config->value;
+        } else {
+            return $default;
+        }
+    }
 }
