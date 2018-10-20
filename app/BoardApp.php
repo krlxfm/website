5bf07ef4ffa21627b72bfec011495ed74a3f8095
Tate Bosler <tatebosler@gmail.com>
2018-10-19 22:34:56 -0500

    fill defaults

--- app/BoardApp.php
+++ app/BoardApp.php
@@ -15,4 +15,23 @@ class BoardApp extends Model
     protected $dispatchesEvents = [
         'creating' => BoardAppCreating::class,
     ];
+
+    /**
+     * The attributes that need type casting.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'common' => 'array',
+        'interview_schedule' => 'array'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'interview',
+    ];
 }
--- app/Listeners/FillBoardAppDefaults.php
+++ app/Listeners/FillBoardAppDefaults.php
@@ -2,6 +2,8 @@
 
 namespace KRLX\Listeners;
 
+use KRLX\Config;
+use Carbon\Carbon;
 use KRLX\Events\BoardAppCreating;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -19,7 +21,20 @@ class FillBoardAppDefaults
         $board_app = $event->board_app;
 
         $board_app->year = date('Y');
-        $board_app->interview_schedule = [];
-        $board_app->common = [];
+
+        $interview_options = json_decode(Config::valueOr('interview options', '[]'), true);
+        $opts = [];
+        foreach($interview_options as $option) {
+            $start = Carbon::parse($option['date'].' '.$option['start'].':00');
+            $end = Carbon::parse($option['date'].' '.$option['end'].':00');
+            $time = $start->copy();
+            while ($time < $end) {
+                $opts[$time->format('Y-m-d H:i:s')] = 0;
+                $time->addMinutes(15);
+            }
+        }
+        $board_app->interview_schedule = $opts;
+        $common = json_decode(Config::valueOr('common questions', '[]'), true);
+        $board_app->common = array_fill_keys($common, '');
     }
 }
