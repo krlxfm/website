84ced97f2c9a53cf5ddf39117d1a8271c9b9c408
Tate Bosler <tatebosler@gmail.com>
2018-12-23 18:05:29 +0000

    Apply fixes from StyleCI

--- app/Console/Commands/IssueXP.php
+++ app/Console/Commands/IssueXP.php
@@ -46,9 +46,9 @@ class IssueXP extends Command
             $this->info('There are no pending experience points.');
 
             return 0;
-        } else if ($dry) {
+        } elseif ($dry) {
             $this->info('Running in dry-run mode. The database will not be affected.');
-        } else if (!$this->confirm('There are experience points pending, would you like to continue?')) {
+        } elseif (! $this->confirm('There are experience points pending, would you like to continue?')) {
             return 0;
         }
 
@@ -72,7 +72,7 @@ class IssueXP extends Command
             if ($eligible) {
                 $issued[] = $point;
             }
-            if (!$dry) {
+            if (! $dry) {
                 $point->status = $eligible ? 'issued' : 'ineligible';
                 $point->save();
             }
