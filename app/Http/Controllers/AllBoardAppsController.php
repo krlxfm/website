0d36b78bcd1f848ebecb0c3256968e000880c2e1
Tate Bosler <tatebosler@gmail.com>
2019-01-17 17:07:03 -0600

    show more stuff on interview screen

--- app/Http/Controllers/AllBoardAppsController.php
+++ app/Http/Controllers/AllBoardAppsController.php
@@ -11,8 +11,8 @@ class AllBoardAppsController extends Controller
 {
     public function index(Request $request)
     {
-        $completed_apps = BoardApp::where([['year', date('Y')], ['submitted', true]])->get();
-        $incomplete_apps = BoardApp::where([['year', date('Y')], ['submitted', false]])->get();
+        $completed_apps = BoardApp::where([['year', date('Y')], ['submitted', true]])->with('user')->get()->sortBy('user.email');
+        $incomplete_apps = BoardApp::where([['year', date('Y')], ['submitted', false]])->with('user')->get()->sortBy('user.email');
         $my_app = $request->user()->board_apps()->where('year', date('Y'))->first();
 
         return view('board.all.index', compact('completed_apps', 'incomplete_apps', 'my_app'));
@@ -34,7 +34,10 @@ class AllBoardAppsController extends Controller
 
     public function interviews()
     {
-        $apps = BoardApp::where([['year', date('Y')], ['submitted', true]])->get();
+        $applications = BoardApp::where([['year', date('Y')], ['submitted', true]])->with('user')->get();
+        $applications = $applications->concat(BoardApp::where([['year', date('Y')], ['submitted', false]])->with('user')->get());
+
+        $apps = $applications->sortBy('user.email');
 
         $interview_options = json_decode(Config::valueOr('interview options', '[]'), true);
         $dates = [];
--- resources/views/board/all/interviews.blade.php
+++ resources/views/board/all/interviews.blade.php
@@ -11,40 +11,41 @@ $colors = ['table', 'table-danger', 'table-warning', 'table-success'];
             <div class="alert alert-info">
                 This screen is designed to work best on a desktop computer. Mobile devices will likely not display all data correctly.
             </div>
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="col-4">
-            <table class="table">
+            <table class="table table-hover table-responsive">
                 <thead>
-                    <th class="w-75"><br>Candidate &amp; positions</th>
+                    <th>Candidate&nbsp;&amp;&nbsp;positions</th>
+                    @foreach($dates as $date)
+                        @if (! $loop->first and $loop->index % 8 == 0)
+                            <th>Candidate&nbsp;&amp;&nbsp;positions</th>
+                        @endif
+                        <th class="text-center">{!! $date->format('D&\n\b\s\p;n/j, H:i') !!}</th>
+                    @endforeach
                 </thead>
                 <tbody>
                     @foreach($apps as $app)
+                        @if (! $loop->first and $loop->index % 12 == 0)
+                            <th>Candidate&nbsp;&amp;&nbsp;positions</th>
+                            @foreach($dates as $date)
+                                @if (! $loop->first and $loop->index % 8 == 0)
+                                    <th>Candidate&nbsp;&amp;&nbsp;positions</th>
+                                @endif
+                                <th class="text-center">{!! $date->format('D&\n\b\s\p;n/j, H:i') !!}</th>
+                            @endforeach
+                        @endif
                         <tr>
-                            <td>{{ $app->user->full_name }}
+                            <td style="white-space: nowrap;" class="{{ $app->submitted ? '' : 'table-warning' }}">{{ $app->user->full_name }}
                                 @foreach($app->positions->pluck('position') as $pos)
                                     <span class="badge badge-{{ $pos->dark ? 'dark' : 'light' }}" style="background: {{ $pos->color }}">{{ $pos->abbr }}</span>
                                 @endforeach
                             </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="col-8">
-            <table class="table table-responsive">
-                <thead>
-                    <tr>
-                        @foreach($dates as $date)
-                            <th class="text-center">{!! $date->format('D&\n\b\s\p;n/j, H:i') !!}</th>
-                        @endforeach
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($apps as $app)
-                        <tr>
                             @foreach($dates as $date)
+                                @if (! $loop->first and $loop->index % 8 == 0)
+                                    <td class="text-center {{ $app->submitted ? '' : 'table-warning' }}" style="white-space: nowrap;">{{ $app->user->full_name }}
+                                        @foreach($app->positions->pluck('position') as $pos)
+                                            <span class="badge badge-{{ $pos->dark ? 'dark' : 'light' }}" style="background: {{ $pos->color }}">{{ $pos->abbr }}</span>
+                                        @endforeach
+                                    </td>
+                                @endif
                                 <td class="text-center {{ $colors[$app->interview_schedule[$date->format('Y-m-d H:i:s')]] }}">
                                     <input type="radio" name="{{ $app->user->email }}" value="{{ $date->format('Y-m-d H:i:s') }}">
                                 </td>
