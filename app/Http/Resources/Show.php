2ee0ce9fb537a2e81ae0cef2eb71f7fa924b39a5
Tate Bosler <tatebosler@gmail.com>
2019-01-20 19:04:36 -0600

    fix bandemonium bug

--- app/Http/Resources/Show.php
+++ app/Http/Resources/Show.php
@@ -32,9 +32,10 @@ class Show extends JsonResource
         ];
 
         $track_managers = Term::orderByDesc('on_air')->first()->track_managers;
+        $extra_hosts = $this->track->weekly ? [] : $track_managers[$this->track->id];
 
         $date = [
-            'djs' => User::whereIn('id', $track_managers[$this->track->id])->get()->pluck('full_name'),
+            'djs' => User::whereIn('id', $extra_hosts)->get()->pluck('full_name'),
             'date' => $this->date,
             'start' => $this->date ? $this->track->start_time : null,
             'end' => $this->date ? $this->track->end_time : null,
