1accd6275e99201e08ac522ae553b0a42cf3df59
Tate Bosler <tatebosler@gmail.com>
2019-01-30 11:16:18 -0600

    add buttons and icons to interview scheduler

--- resources/views/board/all/interviews.blade.php
+++ resources/views/board/all/interviews.blade.php
@@ -2,41 +2,55 @@
 
 @php
 $colors = ['', 'table-danger', 'table-warning', 'table-success'];
+function checkedIfTime($app, $time) {
+    return $app->interview === $time ? 'checked' : '';
+}
 @endphp
 
 @section('mid')
     <div class="row">
         <div class="col">
-            <table class="table table-hover table-responsive">
-                <thead>
-                    <th>Candidate</th>
-                    <th>Positions</th>
-                    <th>Unscheduled</th>
-                    @foreach($dates as $date)
-                        <th class="text-center">{!! $date->format('D&\n\b\s\p;n/j, H:i') !!}</th>
-                    @endforeach
-                </thead>
-                <tbody>
-                    @foreach($apps as $app)
-                        <tr>
-                            <td style="white-space: nowrap;">{{ $app->user->full_name }}</td>
-                            <td style="white-space: nowrap;">
-                                @foreach($app->positions->pluck('position') as $pos)
-                                    <span class="badge badge-{{ $pos->dark ? 'dark' : 'light' }}" style="background: {{ $pos->color }}">{{ $pos->abbr }}</span>
-                                @endforeach
-                            </td>
-                            <td class="text-center">
-                                <input type="radio" name="interviews[{{ $app->id }}]" value="null">
-                            </td>
-                            @foreach($dates as $date)
-                                <td class="text-center {{ $colors[$app->interview_schedule[$date->format('Y-m-d H:i:s')]] }}">
-                                    <input type="radio" name="interviews[{{ $app->id }}]" value="{{ $date->format('Y-m-d H:i:s') }}">
+            <form method="post">
+                @csrf
+                <table class="table table-hover table-responsive">
+                    <thead>
+                        <th>Candidate</th>
+                        <th>Positions</th>
+                        <th>Unscheduled</th>
+                        @foreach($dates as $date)
+                            <th class="text-center">{!! $date->format('D&\n\b\s\p;n/j, H:i') !!}</th>
+                        @endforeach
+                    </thead>
+                    <tbody>
+                        @foreach($apps as $app)
+                            <tr>
+                                <td style="white-space: nowrap;">
+                                    {{ $app->user->full_name }}
+                                    @if ($app->remote)
+                                        <i class="fas fa-video"></i>
+                                    @endif
                                 </td>
-                            @endforeach
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                <td style="white-space: nowrap;">
+                                    @foreach($app->positions->pluck('position') as $pos)
+                                        <span class="badge badge-{{ $pos->dark ? 'dark' : 'light' }}" style="background: {{ $pos->color }}">{{ $pos->abbr }}</span>
+                                    @endforeach
+                                </td>
+                                <td class="text-center">
+                                    <input type="radio" name="interviews[{{ $app->id }}]" value="null" {{ checkedIfTime($app, null) }}>
+                                </td>
+                                @foreach($dates as $date)
+                                    <td class="text-center {{ $colors[$app->interview_schedule[$date->format('Y-m-d H:i:s')]] }}">
+                                        <input type="radio" name="interviews[{{ $app->id }}]" value="{{ $date->format('Y-m-d H:i:s') }}" {{ checkedIfTime($app, $date->format('Y-m-d H:i:s')) }}>
+                                    </td>
+                                @endforeach
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <p>
+                    <button type="submit" class="btn btn-lg btn-primary">Save</button>
+                </p>
+            </form>
         </div>
     </div>
 @endsection
