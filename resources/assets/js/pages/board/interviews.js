e6174ed6d94aedf49e1f1e16ff6fe0a1066e88a9
Tate Bosler <tatebosler@gmail.com>
2019-02-04 01:06:41 -0600

    allow clicking anywhere in table cell

--- resources/assets/js/pages/board/interviews.js
+++ resources/assets/js/pages/board/interviews.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
+    $('td.table-success, td.table-warning, td.table-danger').on('click', clickInternalInput);
     $("#save-with-email").click(saveAndNotifyCandidates);
 });
 
 function saveAndNotifyCandidates() {
     $("#notify-field").val("1");
     $("#interviews-form").submit();
+}
+
+function clickInternalInput() {
+    $(this).find('input').prop("checked", true);
 }
