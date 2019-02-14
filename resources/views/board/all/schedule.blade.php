83582d482444bf03a628d877689496be1e1aeb2e
Tate Bosler <tatebosler@gmail.com>
2019-02-13 21:27:45 -0600

    remove header

--- resources/views/board/all/schedule.blade.php
+++ resources/views/board/all/schedule.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="row">
         <div class="col">
-            <h1>Interview Schedule</h1>
             @foreach($dates as $key => $times)
                 <div class="{{ $loop->first ? '' : 'pdf-page-break'}}">
                     <h3 class="mb-3">{{ \Carbon\Carbon::parse($key)->format('l, F j, Y') }}</h3>
