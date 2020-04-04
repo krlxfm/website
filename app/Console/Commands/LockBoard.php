1e8bbad52fcb3b884d0dfa2e82d03d7965156399
Oliver Calder <oacalder@gmail.com>
2020-04-03 19:39:32 -0500

    Made LockBoard filter users by permissions and print more verbose messaging

--- app/Console/Commands/LockBoard.php
+++ app/Console/Commands/LockBoard.php
@@ -35,9 +35,16 @@ class LockBoard extends Command
         }
         $this->info('Identifying eligible applicants...');
         $users = User::all();
+        $bar = $this->output->createProgressBar($users->count());
+        $bar->start();
         $eligible = $users->filter(function ($user) {
+            $bar->advance();
+
             return $user->hasPermissionTo('apply for board seats');
         });
+        $bar->finish();
+        $this->line('');
+
         $this->info('Revoking permissions...');
         $bar = $this->output->createProgressBar($eligible->count());
         $bar->start();
