95d0bf13e12a11ecb1dcbf3979b6a6e35f27d7dc
Tate Bosler <ichiefboz@users.noreply.github.com>
2021-09-23 05:23:53 +0000

    Apply fixes from StyleCI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!$this->app->environment('local')) {
+        if (! $this->app->environment('local')) {
             URL::forceScheme('https');
         }
 
