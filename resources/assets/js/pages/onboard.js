0029dcc0ca0e7256af3654eb9a4cb60ce8ec0066
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2026-01-06 14:20:52 -0600

    fix class year validation so that staff and faculty can actually sign up

--- resources/assets/js/pages/onboard.js
+++ resources/assets/js/pages/onboard.js
@@ -1,13 +1,20 @@
-$(document).ready(function() {
-    if($('input[name="year"]').val() < 1000) {
+$(document).ready(function () {
+    if ($('input[name="year"]').val() < 1000) {
         $("#classyear-row").hide();
+        $('input[name="year"]').prop("disabled", true);
     }
-    $("#phone_number").mask('999-999-9999', {placeholder:" "});
-    $('input[name="status"]').on('change', toggleClassYearRow);
+    $("#phone_number").mask("999-999-9999", { placeholder: " " });
+    $('input[name="status"]').on("change", toggleClassYearRow);
 });
 
 function toggleClassYearRow() {
     console.log($(this));
     console.log($(this).val());
-    $("#classyear-row").toggle($(this).val() == 'student');
+    if ($(this).val() == "student") {
+        $("#classyear-row").show();
+        $('input[name="year"]').prop("disabled", false);
+    } else {
+        $("#classyear-row").hide();
+        $('input[name="year"]').prop("disabled", true);
+    }
 }
